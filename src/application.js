6ce41d00a71e3b09371df33a9e60024ad3b117ca
Sam Shapiro <sam.shapiro@boweryvaluation.com>
2020-03-24 22:02:56 -0400

    style changes

---
+++ src/application.js
@@ -0,0 +1,45 @@
+const green = require("chalk").green
+const inquirer = require("inquirer")
+
+import { isNil } from 'lodash'
+
+import mixCoins from './mixer'
+import * as utils from './utils'
+
+const startApplication = async () => {
+  const depositAddress = utils.generateDepositAddress()
+  let answers = await inquirer.prompt([
+    {
+      name: "addresses",
+      message: "Please enter a comma-separated list of new, unused Jobcoin addresses where your mixed Jobcoins will be sent:",
+    }
+  ])
+  
+  let addresses = []
+  const addressesString = answers.addresses
+  if(!isNil(addressesString) && addressesString !== ''){
+    addresses = addressesString.replace(', ', ',').split(',')
+  }
+
+  console.log(`You may now send Jobcoins to address ${green(depositAddress)}. They will be mixed and sent to your destination addresses.  This operation will timeout in 30 seconds.`)
+  console.log('Mixing coins')
+  await mixCoins(depositAddress, addresses)
+  console.log('finished mixing coins!')
+
+  answers = await inquirer.prompt([
+    {
+      name: "continue",
+      message: `Enter ${green('"y"')} to run again`
+    },
+  ])
+
+  if (answers.continue && answers.continue.toLowerCase() === "y") {
+    await prompt();
+  }
+}
+
+export default startApplication
+
+
+
+
---
+++ src/mixer.js
@@ -0,0 +1,83 @@
+
+import * as utils from './utils'
+import * as api from './apiClient'
+
+import { isNil, isEmpty } from 'lodash'
+
+const MIXER_TIMEOUT = 30000
+const MIXER_INTERVAL = 1000
+
+const HOUSE_ADDRESS = 'THE_HOUSE'
+
+
+const mixCoins = (depositAddress, outAddresses) => new Promise(async (resolve, reject) => {
+  if (isNil(depositAddress) || isEmpty(outAddresses)) {
+    reject()
+  }
+  
+  const depAddrTransactions = await api.transactionsForAddress(depositAddress) || []
+  let elapsed = 0;
+  const interval = setInterval(async () => {
+    
+    const depAddrCurrentInfo = await api.infoForAddress(depositAddress) || {}
+    const depAddrCurrentTransactions = depAddrCurrentInfo.transactions || []
+
+    if(!isEmpty(depAddrCurrentTransactions) && depAddrCurrentTransactions.length > depAddrTransactions.length){
+      console.log('Deposit Address Received Coins!');
+      await api.transfer(depositAddress, HOUSE_ADDRESS, depAddrCurrentInfo.balance)
+      await disburseCoins(outAddresses, depAddrCurrentInfo.balance);
+
+      clearInterval(interval)
+      resolve()
+    }
+
+    if(elapsed > MIXER_TIMEOUT){
+      console.log('Timeout occurred')
+      clearInterval(interval)
+      reject()
+    }
+
+    elapsed += MIXER_INTERVAL
+  }, MIXER_INTERVAL);
+})
+
+const disburseCoins = async (outAddresses, amount) => {
+  if(amount <= 0){
+    return
+  }
+
+  const disbursements = outAddresses.map(address => {
+    return {
+      address,
+      amount: 0,
+    }
+  })
+
+  console.log('preparing disbursement amounts')
+  let remainingToDisburse = amount
+  while(remainingToDisburse > 0) {
+    const idx = utils.randomInteger(0, disbursements.length - 1);
+    disbursements[idx].amount += 1
+    remainingToDisburse -= 1
+  }
+
+  console.log('Prepared the following: ')
+  console.log(disbursements)
+
+  const houseAddrInfo = await api.infoForAddress(HOUSE_ADDRESS) || {}
+  const houseAddrBalance = houseAddrInfo.balance ? parseInt(houseAddrInfo.balance) : 0
+  if(houseAddrBalance < amount){
+    console.log('House is BROKE!  Cannot make disbursements')
+    return;
+  }
+
+  for (const disbursement of disbursements) {
+    if (disbursement.amount > 0) {
+      await api.transfer(HOUSE_ADDRESS, disbursement.address, disbursement.amount)
+    }
+  }
+}
+
+export default mixCoins;
+
+
