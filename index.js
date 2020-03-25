5c25ce8669acf4ef0b4f22fa50e347848d63a450
Sam Shapiro <sam.shapiro@boweryvaluation.com>
2020-03-24 21:22:48 -0400

    it's functionally correct.  the prompts are screwy

--- index.js
+++ index.js
@@ -7,35 +7,41 @@ import mixCoins from './mixer'
 import * as utils from './utils'
 
 import { isNil } from 'lodash'
-
 const prompt = async () => {
   const depositAddress = utils.generateDepositAddress()
-  const answers = await inquirer.prompt([
+  let answers = await inquirer.prompt([
     {
       name: "addresses",
       message: "Please enter a comma-separated list of new, unused Jobcoin addresses where your mixed Jobcoins will be sent:",
-    },
-    {
-      name: "deposit",
-      message: `You may now send Jobcoins to address ${green(depositAddress)}. They will be mixed and sent to your destination addresses. \n Enter ${green('"y"')} to run again.`,
-      when: (answers) => { 
-        let addresses = answers.addresses
-        if(!isNil(addresses) && addresses !== ''){
-          addresses = addresses.replace(', ', ',').split(',')
-          mixCoins(depositAddress, addresses)
-        }
-        return answers.addresses
-      }
-    },
+    }
   ])
   
-  if (answers.deposit && answers.deposit.toLowerCase() === "y") {
+  console.log(`You may now send Jobcoins to address ${green(depositAddress)}. They will be mixed and sent to your destination addresses.  This operation will timeout in 30 seconds.`)
+
+  let addresses = answers.addresses
+  if(!isNil(addresses) && addresses !== ''){
+    console.log('Mixing coins')
+    addresses = addresses.replace(', ', ',').split(',')
+    await mixCoins(depositAddress, addresses)
+    console.log('finished mixing coings!')
+  } else {
+    console.log('NOT Mixing coins')
+  }
+
+  answers = await inquirer.prompt([
+    {
+      name: "continue",
+      message: `Enter ${green('"y"')} to run again`
+    },
+  ])
+
+  if (answers.continue && answers.continue.toLowerCase() === "y") {
     await prompt();
   }
 }
 
 console.log("Welcome to the Jobcoin mixer!");
-prompt();
+prompt()
 
 
 
